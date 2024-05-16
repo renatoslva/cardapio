c2670e83b885f1806360c1284101b2e7c5af1253
RENATODEVPROENÇA <101299005+renatoslva@users.noreply.github.com>
2024-05-15 23:15:44 -0300

    começando-o-script

---
+++ script.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById("menu1")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItems = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCount = document.getElementById("cart-count")
+const addressInput = document.getElementById("addreess")
+const addressWarn = document.getElementById("address-warn")
+
+cartBtn.addEventListener('click', function(){
+    cartModal.style.display = "flex"
+})
+
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+console.log(cartModal)
